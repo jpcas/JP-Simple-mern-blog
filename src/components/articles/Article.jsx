3bde186e00176fe2110b27030175e893b2410e97
JP Castrillon <jpcas001@gmail.com>
2020-11-08 19:37:57 -0500

    weekendproject

---
+++ src/components/articles/Article.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+
+const Article = ({ match }) => {
+    
+    const [article, setArticle] = useState({});
+
+    useEffect(() => {
+        fetch(`/articles/${match.params.articleID}`)
+            .then(response => response.json())
+            .then(article => setArticle(article))
+            .catch(error => alert(error))
+    });
+    return (
+        <article>
+            <h1>{article.title}</h1>
+            <p>{article.text}</p>
+        </article>
+    );
+};
+
+export default Article;
